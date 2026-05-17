a98078404e169137a4d71f32a8f66ae7ce37148a
cathhzeee <cathyzll1504@gmail.com>
2026-05-17 16:50:42 +0700

    maze generator catherine

---
+++ MazeGeneratorCath/Block.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Block {
+    int row;
+    int col;
+    boolean visited = false;
+
+    // top, right, bottom, left
+    boolean[] walls = {true, true, true, true};
+
+    // for storing neighbors
+    ArrayList<Block> neighbors = new ArrayList<>();
+
+    // for picking random neighbors
+    Random random = new Random();
+
+    public Block(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public void addNeighbors(Block[][] blocks, int rows, int cols) {
+        // top neighbor
+        if(row > 0) {
+            neighbors.add(blocks[row - 1][col]);
+        }
+
+        // right neighbor
+        if(col < cols - 1) {
+            neighbors.add(blocks[row][col + 1]);
+        }
+
+        // bottom neighbor
+        if(row < rows - 1) {
+            neighbors.add(blocks[row + 1][col]);
+        }
+
+        // left neighbor
+        if(col > 0) {
+            neighbors.add(blocks[row][col - 1]);
+        }
+    }
+
+    // check if there are unvisited neighbors
+    public boolean hasUnvisitedNeighbors() {
+        for(Block n : neighbors) {
+            if(!n.visited) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // pick a random unvisited neighbor
+    public Block pickRandomNeighbor() {
+        ArrayList<Block> unvisited = new ArrayList<>();
+        for(Block n : neighbors) {
+            if(!n.visited) {
+                unvisited.add(n);
+            }
+        }
+
+        if(unvisited.size() > 0) {
+            Block chosen = unvisited.get(random.nextInt(unvisited.size()));
+            chosen.visited = true;
+            return chosen;
+        }
+        return null;
+    }
+}
