923721214a4a6b5a0bf0745b71673806481a7fc6
Michael <michaelrespatis@gmail.com>
2026-05-25 20:19:04 +0700

    Add Dijkstra and A* by Michael

--- src/main/MazeFigureTest.java
+++ src/main/MazeFigureTest.java
@@ -1,30 +1,33 @@
 package main;
 
 import MazeGeneratorCath.MazeMaker;
-import graph.BTNode;
 import graph.Graph;
-import graph.MyLinearList;
 import visual.MazeFrame;
 
 public class MazeFigureTest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int row = 100;
-		int col = 100;
-		
-		MazeFrame kFrame =  new MazeFrame(row, col);
-		
-		MazeMaker k = kFrame.getMazeMaker();
-		
-		Graph<Integer> mGraph = k.toGraph();
-		mGraph.printGraph();
-		
-		BTNode<Integer, MyLinearList<Integer>> retBtNode = mGraph.dijkstra(0, row*col - 1);
-		
-		System.out.println("Cost: " + retBtNode.getKey());
-		System.out.println("Path: ");
-		retBtNode.getData().cetakList();
-	}
+    public static void main(String[] args) {
 
+        int row = 20;
+        int col = 20;
+
+        MazeFrame kFrame = new MazeFrame(row, col);
+
+        MazeMaker k = kFrame.getMazeMaker();
+
+        Graph<Integer> mGraph = k.toGraph();
+
+        int start = 0;
+
+        int target = row * col - 1;
+
+        // jalankan Dijkstra
+        mGraph.dijkstraReport(start, target);
+
+        System.out.println();
+
+        // jalankan A*
+        // col dipakai untuk menghitung row dan col dari index vertex
+        mGraph.aStarReport(start, target, col);
+    }
 }
